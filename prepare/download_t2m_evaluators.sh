477c627bbfa2c3c7afd225361fa3ee6d960adb37
Jackie Ye <54547626+JYe16@users.noreply.github.com>
2025-10-24 09:55:57 -0400

    bug fix

--- prepare/download_t2m_evaluators.sh
+++ prepare/download_t2m_evaluators.sh
@@ -1,8 +1,6 @@
 mkdir -p deps/
 cd deps/
-
 echo "The t2m evaluators will be stored in the './deps' folder"
-
 # HumanAct12 poses
 echo "Downloading"
 gdown "https://drive.google.com/uc?id=1AYsmEG8I3fAAoraT4vau0GnesWBWyeT8"
@@ -10,5 +8,4 @@ echo "Extracting"
 tar xfzv t2m.tar.gz
 echo "Cleaning"
 rm t2m.tar.gz
-
 echo "Downloading done!"
